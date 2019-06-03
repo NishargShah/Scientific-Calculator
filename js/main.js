ddde9043f93070c43b545d89501e5966a2935e41
NishargShah <nishargshah3101@gmail.com>
2019-06-04 00:51:15 +0530

    make whole array

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 let UIController = (() => {
     // ALL DOMData
     const DOMData = {
-        calculator: document.querySelector('.calculator'),
         displayTop: document.querySelector('.display_top'), displayBottom: document.querySelector('.display_bottom'),
         on: document.querySelector('.on'), equal: document.querySelector('.equal'),
         shift: document.querySelector('.shift'), circleShift: document.querySelector('.circle_shift'),
@@ -15,22 +14,56 @@ let UIController = (() => {
         addition: document.querySelector('.addition'), subtraction: document.querySelector('.subtraction'),
         multiply: document.querySelector('.multiply'), divide: document.querySelector('.divide'),
         point: document.querySelector('.point'), exp: document.querySelector('.exp'), shiftCount: 0, alphaCount: 0
-
-        // displayTop displayBottom shiftCount alphaCount delete allClear
     };
 
-    // ALL VALUES OF [CLASSNAME, ORG, SHIFT, ALPHA]
+    // ALL VALUES OF [DISPLAY, shiftDisplay, alphaDisplay, CLASSNAME, ORG, SHIFT, ALPHA]
     const dataContent = [
-        ['shift', 'Shift'], ['alpha', 'Alpha'], ['mode', 'Mode', 'CLR'], ['on', 'On'],
-        ['x_inverse', 'x<sup>-1</sup>', 'x!'], ['combinations', 'nCr', 'nPr'], ['pol', 'Pol(', 'Rec(', ':'], ['x_cube', 'x<sup>3</sup>', '<sup>3</sup>√'],
-        ['abc', 'a b/c', 'd/c'], ['sqrt', '√'], ['x_square', 'x<sup>2</sup>'], ['up_arrow', '^', '<sup>x</sup>√'], ['log', 'log', '10<sup>x</sup>'], ['ln', 'ln', 'e<sup>x</sup>', 'e'],
-        ['bracket_minus', '(-)', null, 'A'], ['degree', '. , , ,', '<-', 'B'], ['hyp', 'hyp', null, 'C'],
-        ['sin', 'sin', 'sin<sup>-1</sup>', 'D'], ['cos', 'cos', 'cos<sup>-1</sup>', 'E'], ['tan', 'tan', 'tan<sup>-1</sup>', 'F'],
-        ['rcl', 'RCL', 'STO'], ['eng', 'ENG', '<-'], ['left_bracket', '('], ['right_bracket', ')', null, 'X'], ['coma', ',', ';', 'Y'], ['m_plus', 'M+', 'M-', 'M'],
-        ['seven', '7'], ['eight', '8'], ['nine', '9'], ['delete', 'DEL', 'INS'], ['all_clear', 'AC', 'OFF'],
-        ['four', '4'], ['five', '5'], ['six', '6'], ['multiply', '×'], ['divide', '÷'],
-        ['one', '1', 'S-SUM'], ['two', '2', 'S-VAR'], ['three', '3'], ['addition', '+'], ['subtraction', '-'],
-        ['zero', '0', 'Rnd'], ['point', '.', 'Ran#'], ['exp', 'EXP', 'π'], ['answer', 'Ans', 'DRG>'], ['equal', '=', '%']
+        [null, null, null, 'shift', 'Shift'],
+        [null, null, null, 'alpha', 'Alpha'],
+        [null, null, null, 'mode', 'Mode', 'CLR'],
+        [null, null, null, 'on', 'On'],
+        ['⁻¹', '!', '⁻¹', 'x_inverse', 'x<sup>-1</sup>', 'x!'],
+        [null, null, null, 'combinations', 'nCr', 'nPr'],
+        [null, null, null, 'pol', 'Pol(', 'Rec(', ':'],
+        ['³', '³√', '³', 'x_cube', 'x<sup>3</sup>', '<sup>3</sup>√'],
+        [null, null, null, 'abc', 'a b/c', 'd/c'],
+        ['√', '√', '√', 'sqrt', '√'],
+        ['²', '²', '²', 'x_square', 'x<sup>2</sup>'],
+        ['^', 'ˣ√', '^', 'up_arrow', '^', '<sup>x</sup>√'],
+        ['log ', '₁₀', 'log ', 'log', 'log', '10<sup>x</sup>'],
+        ['ln ', 'ₑ', 'e', 'ln', 'ln', 'e<sup>x</sup>', 'e'],
+        [null, null, 'A', 'bracket_minus', '(-)', null, 'A'],
+        [null, null, 'B', 'degree', '. , , ,', '<-', 'B'],
+        [null, null, 'C', 'hyp', 'hyp', null, 'C'],
+        ['sin ', 'sin⁻¹', 'D', 'sin', 'sin', 'sin<sup>-1</sup>', 'D'],
+        ['cos ', 'cos⁻¹', 'E', 'cos', 'cos', 'cos<sup>-1</sup>', 'E'],
+        ['tan ', 'tan⁻¹', 'F', 'tan', 'tan', 'tan<sup>-1</sup>', 'F'],
+        [null, null, null, 'rcl', 'RCL', 'STO'],
+        [null, null, null, 'eng', 'ENG', '<-'],
+        ['( ', '( ', '( ', 'left_bracket', '('],
+        [' )', ' )', 'X', 'right_bracket', ')', null, 'X'],
+        [null, null, null, 'coma', ',', ';', 'Y'],
+        [null, null, null, 'm_plus', 'M+', 'M-', 'M'],
+        ['7', '7', '7', 'seven', '7'],
+        ['8', '8', '8', 'eight', '8'],
+        ['9', '9', '9', 'nine', '9'],
+        [null, null, null, 'delete', 'DEL', 'INS'],
+        [null, null, null, 'all_clear', 'AC', 'OFF'],
+        ['4', '4', '4', 'four', '4'],
+        ['5', '5', '5', 'five', '5'],
+        ['6', '6', '6', 'six', '6'],
+        ['*', '*', '*', 'multiply', '×'],
+        ['/', '/', '/', 'divide', '÷'],
+        ['1', '', '', 'one', '1', 'S-SUM'],
+        ['2', '', '', 'two', '2', 'S-VAR'],
+        ['3', '3', '3', 'three', '3'],
+        ['+', '+', '+', 'addition', '+'],
+        ['-', '-', '-', 'subtraction', '-'],
+        ['0', 'Rnd', '0', 'zero', '0', 'Rnd'],
+        ['.', 'Ran#', '.', 'point', '.', 'Ran#'],
+        ['E', 'π', 'E', 'exp', 'EXP', 'π'],
+        [null, null, null, 'answer', 'Ans', 'DRG>'],
+        [null, '%', null, 'equal', '=', '%']
     ];
 
     // RETURN OF UIController
@@ -50,10 +83,10 @@ let updateController = (ui => {
             // FOREACH FOR GET LIST OF IT
             selectAllButton.forEach(cur => {
                 // FETCH DATA FROM UIController
-                let getDataClassName = ui.getDataContent[i][0];
-                let getDataORG = ui.getDataContent[i][1];
-                let getShiftData = ui.getDataContent[i][2];
-                let getAlphaData = ui.getDataContent[i][3];
+                let getDataClassName = ui.getDataContent[i][3];
+                let getDataORG = ui.getDataContent[i][4];
+                let getShiftData = ui.getDataContent[i][5];
+                let getAlphaData = ui.getDataContent[i][6];
 
                 if (value === 'shift') {
                     // IF DATA IS UNDEFINED OR NULL, SECOND VALUE = FIRST VALUE
@@ -87,8 +120,7 @@ let updateController = (ui => {
 
     // FIRED WHEN YOU CLICK ON "ON"
     let clickOnStart = () => {
-        updateDOM.displayTop.value = '';
-        updateDOM.displayBottom.value = '0';
+        allClear();
         updateDOM.displayTop.style.visibility = 'visible';
         updateDOM.displayBottom.style.visibility = 'visible';
     };
@@ -102,6 +134,16 @@ let updateController = (ui => {
     // FIRED WHEN YOU CLICK ON "DEL"
     let clickOnDelete = () => {
         updateDOM.displayTop.value = updateDOM.displayTop.value.slice(0, -1);
+    };
+
+    // SWAP WHEN CLICK ON ANY BUTTON EXCEPT SHIFT AND ALPHA
+    let swapWhenClickOnButton = (current) => {
+        if (current.target.className !== 'shift' && current.target.className !== 'alpha') {
+            updateDOM.shiftCount = 1;
+            updateDOM.alphaCount = 1;
+            swapOfShiftAndAlpha('shift');
+            swapOfShiftAndAlpha('alpha');
+        }
     };
 
     // FIRE WHEN CLICK ON BUTTON LIKE SHIFT ALPHA
@@ -124,13 +166,43 @@ let updateController = (ui => {
         });
 
         // FIRED WHEN YOU CLICK ON "AC"
-        updateDOM.on.addEventListener('click', () => {
+        updateDOM.allClear.addEventListener('click', () => {
             allClear();
         });
 
         // FIRED WHEN YOU CLICK ON "DEL"
         updateDOM.delete.addEventListener('click', () => {
             clickOnDelete();
+        });
+
+        // FETCH ALL DATA FROM BUTTONS
+        selectAllButton.forEach(cur => {
+            // CLICK ON BUTTON
+            cur.addEventListener('click', current => {
+
+                swapWhenClickOnButton(current);
+
+                for (let i = 0; i <= selectAllButton.length - 1; i++) {
+                    let getDisplayORG = ui.getDataContent[i][0];
+                    let getDisplayOfShift = ui.getDataContent[i][1];
+                    let getDisplayOfAlpha = ui.getDataContent[i][2];
+                    let getDataClassName = ui.getDataContent[i][3];
+                    let classVar = current.target.className;
+
+                    if (classVar === getDataClassName && getDisplayORG !== null && (updateDOM.shiftCount === 0 && updateDOM.alphaCount === 0)) {
+                        updateDOM.displayTop.value += getDisplayORG;
+                        console.log('press if');
+                    } else {
+                        console.log('press else');
+                        // if (updateDOM.shiftCount === 1) {
+                        //     updateDOM.displayTop.value += getDisplayOfShift;
+                        // }
+                        // if (updateDOM.alphaCount === 1) {
+                        //     updateDOM.displayTop.value += getDisplayOfAlpha;
+                        // }
+                    }
+                }
+            });
         });
     };
 
